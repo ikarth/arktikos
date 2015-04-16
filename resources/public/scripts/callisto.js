fccd0292e1a50368dc6f8808cc5302e90e76ba33
IKarth <isaac@isaackarth.com>
2015-04-16 18:58:14 -0400

    Front end: better date handling

--- resources/public/scripts/callisto.js
+++ resources/public/scripts/callisto.js
@@ -4,12 +4,12 @@ var dataSource = "http://localhost:3000/data";
 function drawTimeChart() {
   var tip = d3.tip().attr("class", "d3-tip")
   .html(function(d) {
-    return d.from + "<br>" + d.subject;
+    return d.from + "<br>" + d.subject + "<br>" + d.date;
   });
 
   var width = 860, height = 500;
 
-  var parseDate = d3.time.format("%Y-%m-%d").parse,
+  var parseDate = d3.time.format("%Y-%m-%d-%H-%M-%S").parse,
       formatPercent = d3.format(".0%");
 
 
@@ -53,7 +53,7 @@ function drawTimeChart() {
     });
 
     data.data.sort(function(a,b) {
-      var dateDiff = a.date-b.date;
+      var dateDiff = d3.time.day(a.date)-d3.time.day(b.date);
       if (dateDiff == 0) {
         dateDiff = a.senderId - b.senderId;
       }
@@ -62,21 +62,26 @@ function drawTimeChart() {
 
     var firstDate = data.data[0].date;
     var lastDate = data.data[data.data.length - 1].date;
-    var curDate = firstDate;
+    var curDate = d3.time.day(firstDate);
     var orderInc = 0;
     data.data.forEach(function(d, i) {
-      if (curDate < d.date) {
+      if (d3.time.day(curDate) < d3.time.day(d.date)) {
         orderInc = 0;
-        curDate = d.date;
+        curDate = d3.time.day(d.date);
       }
       d.order = d.order + orderInc;
       orderInc = orderInc + 1;
     });
 
+
+    var timeExtent = d3.extent(data.data, function(d) { return d3.time.day(d.date); });
+    timeExtent[1].setDate(timeExtent[1].getDate() + 1);
+
     //x = d3.time.scale().domain([firstDate, lastDate]).range([0,width]);
     //x.domain = d3.extent(data.data, function(d) { return d.date; })
     x = d3.time.scale()
-    .domain(d3.extent(data.data, function(d) { return d.date; }))
+    //.domain(d3.extent(data.data, function(d) { return d.date; }))
+    .domain(timeExtent)
     .range([0,width]);
 
     //console.log(d3.extent(data.data, function(d) { return d.date; }));
@@ -86,23 +91,27 @@ function drawTimeChart() {
     y.domain([0, 10]);
 
     //var buckets = d3.time.days(x.domain()[0], new Date().setDate(x.domain()[1].getDate + 1) );
-    var buckets = d3.time.days(x.domain()[0], x.domain()[1]);
-    var cellWidth = d3.scale.ordinal().domain(buckets).rangeRoundBands(x.range(), 0.05).rangeBand();
+    var lastDatePlusOne = new Date(x.domain()[1]);
+    lastDatePlusOne.setDate(lastDatePlusOne.getDate());
+    var buckets = d3.time.days(x.domain()[0], lastDatePlusOne);
+    var cellWidth = d3.scale.ordinal().domain(buckets).rangeRoundBands(x.range(), 0.0).rangeBand();
 
-    //console.log(buckets);
-    //console.log(buckets.length);
-    //console.log(cellWidth);
+    console.log(timeExtent);
+    console.log(timeExtent[1]);
+    console.log(width);
+    console.log(buckets);
+    console.log(buckets.length);
+    console.log(cellWidth);
 
     var cellHeight = 20;
 
     var cell = timechart.selectAll("g")
-    .data(data.data)
-    .enter().append("g");
+    .data(data.data).enter().append("g");
 
     //console.log(data.data);
 
     cell.append("rect")
-    .attr("x", function(d) { return x(d.date); })
+    .attr("x", function(d) { return x(d3.time.day(d.date)); })
     .attr("y", function(d) {return height - (d.order * cellHeight);})
     .attr("height", cellHeight)
     .attr("width", cellWidth)
@@ -135,7 +144,7 @@ function drawTimeChart() {
 }
 
 function drawNodeGraph() {
-  var width = 860, height = 500;
+  var width = 1260, height = 900;
   var color = d3.scale.category20();
   var force = d3.layout.force()
     .charge(-240)
@@ -148,9 +157,9 @@ function drawNodeGraph() {
                   });
 
   var d3cola = cola.d3adaptor()
-    .linkDistance(60)
+    .linkDistance(40)
     .avoidOverlaps(true)
-    .symmetricDiffLinkLengths(15)
+    .symmetricDiffLinkLengths(10)
     .size([width,height]);
 
   var nodeGraph = d3.select("body").append("svg")
